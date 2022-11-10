bdc311f1561715c816ec7a09e1e7d6470c5dd697
Evan Telizhenko <doccccccc384@gmail.com>
2022-11-11 01:37:12 +0200

    lesson 20 - NavLink and his atributes.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import classes from './Navbar.module.css';
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <ul className={classes.list}>
-                <li><Link className={classes.item} to="/profile">Profile</Link></li>
-                <li><Link className={`${classes.item} ${classes.active}`} to="/dialogs">Message</Link></li>
-                <li><Link className={classes.item} to="/news">News</Link></li>
-                <li><Link className={classes.item} to="/music">Music</Link></li>
-                <li><Link className={classes.item} to="/settings">Settings</Link></li>
+                <li className={classes.item}><NavLink className={ navData => navData.isActive ? classes.activeLink : classes.item } to="/profile">Profile</NavLink></li>
+                <li className={classes.item}><NavLink className={ navData => navData.isActive ? classes.activeLink : classes.item } to="/dialogs">Message</NavLink></li>
+                <li className={classes.item}><NavLink className={ navData => navData.isActive ? classes.activeLink : classes.item } to="/news">News</NavLink></li>
+                <li className={classes.item}><NavLink className={ navData => navData.isActive ? classes.activeLink : classes.item } to="/music">Music</NavLink></li>
+                <li className={classes.item}><NavLink className={ navData => navData.isActive ? classes.activeLink : classes.item } to="/settings">Settings</NavLink></li>
             </ul>
         </nav>
     );
